934f03645eee9da42ad292d4989ab3527e6eb698
anm581 <164871930+anm581@users.noreply.github.com>
2024-04-08 12:00:37 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
-
-
-int main() {
-
-
-
-
-
-
-
-
-                                                                
+int main(int argc, char* argv[]) {
+                                                           
     printf("Hello World \n");
     return 0;
 }
